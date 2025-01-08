60d938511152220668cf586abfa8b2c3a773abdd
Tristen <Tman3601@outlook.comgit config --global user.name>
2025-01-08 07:05:48 -0600

    new test bool widget

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,7 +5,13 @@
 package frc.robot;
 
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.ComplexWidget;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.shuffleboard.SimpleWidget;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.RunCommand;
@@ -19,18 +25,39 @@ public class RobotContainer {
   private final XboxController m_driveController = new XboxController(0);
   private final XboxController m_subsystemsController = new XboxController(1);
 
+  private final ShuffleboardTab telopTab = Shuffleboard.getTab("Telop");
+
+  private boolean testBool = false;
+  private Timer timer = new Timer();
+
+  private final SimpleWidget testBoolWidget = telopTab
+  .add("Boolean", testBool)
+  .withWidget(BuiltInWidgets.kBooleanBox);
+
+
   public RobotContainer() {
 
     configureBindings();
 
     configureDefaultCommands();
+    timer.start();
   }
 
   public void periodic() {
-    
+    if (timer.get() % 2 < 1) {
+      testBool = true;
+    } else {
+      testBool = false;
+    }
+
+    testBoolWidget.getEntry().setBoolean(testBool);
   }
 
   private void configureBindings() {
+    
+  }
+
+  private void configureDashboard() {
     
   }
 
